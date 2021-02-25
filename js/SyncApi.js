ed531c28e7a823731dbd2820c233e70562107677
hibarike <hibarike@gmail.com>
2021-02-25 22:31:35 +0200

    SyncApi

--- js/SyncApi.js
+++ js/SyncApi.js
@@ -1,56 +1,38 @@
-//--------------------SyncApi
-var login = 116448;
-var time = Math.floor(Date.now() / 1000);
-var secret = "JalPRClXtArX6tDUkU26WvVXFSWrgRjuCbZAW0w4561Avw9i38";
-var data = {
-  timeout: 30,
-  ops: [
-    {
-      conv_id: 904246,
-      type: "create",
-      obj: "task",
-      data: {
-        param: 1
-      }
-    }
-  ]
-};
-var CryptoJS = require("crypto-js");
-var signature = CryptoJS.enc.Hex.stringify(
-  CryptoJS.SHA1(time + secret + data + secret)
-);
-const url =
-  "https://sync-api.corezoid.com/api/1/json/" +
-  login +
-  "/" +
-  time +
-  "/" +
-  signature;
+var CryptoJS = require("crypto-js");
+var axios = require("axios");
 
-// Пример отправки POST запроса:
-async function postData(url = "", data = {}) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "no-cors", // no-cors, *cors, same-origin
-    //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    //credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      "Content-Type": "application/json"
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    //redirect: 'follow', // manual, *follow, error
-    // referrerPolicy: 'no-referrer', // no-referrer, *client
-    body: data // body data type must match "Content-Type" header
-  });
-  return await response.json(); // parses JSON response into native JavaScript objects
-}
-
-postData(url, data).then((data) => {
-  console.log(data); // JSON data parsed by `response.json()` call
+var login = "116353";
+var time = Math.floor(Date.now() / 1000);
+var secret = "7xkSDPSqg0qprb1hWGelVSojJGyJYPeFsz7gkKMX8LS2PZPyc6";
+var data = JSON.stringify({
+  timeout: 30,
+  ops: [{ conv_id: 901908, type: "create", obj: "task", data: {"param": "1"} }]
 });
 
+var signature = CryptoJS.enc.Hex.stringify(
+  CryptoJS.SHA1(time + secret + data + secret)
+);
+const url = `https://sync-api.corezoid.com/api/1/json/${login}/${time}/${signature}`;
 
+console.log(url)
 
+const axiosConfig = {
+  method: 'HEAD',
+		mode: 'no-cors',
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+		},
+		withCredentials: true,
+		credentials: 'same-origin',
+		crossdomain: true,
+}
 
+async function makeGetRequest() {
+  let res = await axios.post(url, data, axiosConfig);
+  let d = res.data;
+  console.log(d);
+}
 
+makeGetRequest();
